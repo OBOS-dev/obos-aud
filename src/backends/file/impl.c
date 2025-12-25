dbecc9219aeaa5ad23fa4ec09d9bcb7396f9e764
Omar Berrow <omarkberrow@gmail.com>
2025-12-25 12:58:49 -0500

    backends/file: implement aud_backend_initialize and aud_backend_get_outputs

--- src/backends/file/impl.c
+++ src/backends/file/impl.c
@@ -5,15 +5,42 @@
  */
 
 #include <obos-aud/compiler.h>
+#include <obos-aud/output.h>
 
 #include <obos-aud/priv/backend.h>
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+#include <sys/param.h>
+
+static int s_backend_file_output = -1;
+#define OUTPUT_COUNT 1
+static aud_output_dev s_backend_outputs[OUTPUT_COUNT] = {
+    {
+        .type = OBOS_AUD_OUTPUT_TYPE_SPEAKER,
+        .color = OBOS_AUD_OUTPUT_COLOR_UNKNOWN,
+        .location = OBOS_AUD_OUTPUT_LOCATION_UNKNOWN,
+        .pad = {},
+        .flags = OBOS_AUD_OUTPUT_FLAGS_DEFAULT,
+        .output_id = 1,
+    }
+};
+
 int aud_backend_initialize()
 {
+    s_backend_file_output = open("obos-aud-output", O_CREAT|O_TRUNC|O_WRONLY);
+    if (s_backend_file_output < 0)
+        return s_backend_file_output;
     return 0;
 }
+
 // Returns the amount of outputs, or -1 on error
 int aud_backend_get_outputs(aud_output_dev* arr, int count)
 {
-    return -1;
+    if (!count)
+        return OUTPUT_COUNT;
+    memcpy(arr, s_backend_outputs, MIN(count, OUTPUT_COUNT));
+    return OUTPUT_COUNT;
 }
