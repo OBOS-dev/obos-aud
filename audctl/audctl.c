7e109521641a27b5d7d8d2098ba81ff28795523c
Omar Berrow <omarkberrow@gmail.com>
2025-12-30 14:15:50 -0500

    audctl: add volume control commands

--- audctl/audctl.c
+++ audctl/audctl.c
@@ -10,8 +10,23 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <strings.h>
+#include <stdlib.h>
+#include <errno.h>
 
-const char* usage = "%s [-d server_uri] [-h] command...";
+const char* usage = "%s [-d server_uri] [-h] command...\n";
+
+static void print_volume(float volume)
+{
+    printf("volume=%f%%\n|", volume);
+    for (int i = 0; i < (int)(volume > 100 ? 99 : volume); i++)
+        printf("#");
+    for (int i = 0; i < (100-(int)volume); i++)
+        printf("-");
+    if (volume > 100)
+        printf("*");
+    printf("|\n");
+}
 
 int main(int argc, char** argv)
 {
@@ -53,7 +68,7 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    struct aud_packet reply = {};
+    aud_packet reply = {};
     if (autrans_receive(socket, &reply, NULL, 0) < 0)
     {
         shutdown(socket, SHUT_RDWR);
@@ -63,6 +78,105 @@ int main(int argc, char** argv)
     }
     client_id = reply.client_id;
 
+    const char* command = argv[optind];
+    const char* const* command_argv = (optind+1) < argc ? (const char**)&argv[optind+1] : NULL;
+    size_t command_argc = argc - (optind+1);
+
+    if (strcasecmp(command, "output-set-volume") == 0)
+    {
+        if (command_argc < 1)
+        {
+            fprintf(stderr, "output-set-volume [output_id] volume\n");
+            goto die;
+        }
+        uint16_t output_id = OBOS_AUD_DEFAULT_OUTPUT_DEV;
+        float volume = 0;
+        if (command_argc == 2)
+        {
+            errno = 0;
+            output_id = strtoul(command_argv[0], NULL, 0);
+            if (errno != 0)
+            {
+                fprintf(stderr, "Expected unsigned integer, got %s\n", command_argv[0]);
+                goto die;
+            }
+        }
+        errno = 0;
+        volume = strtof(command_argv[command_argc == 2 ? 1 : 0], NULL);
+        if (errno != 0 || volume < 0)
+        {
+            fprintf(stderr, "Expected floating point integer, got %s\n", command_argv[0]);
+            goto die;
+        }
+
+        if (autrans_output_set_volume(socket, client_id, output_id, volume) == 0)
+            print_volume(volume);
+    }
+    else if (strcasecmp(command, "connection-set-volume") == 0)
+    {
+        if (command_argc < 2)
+        {
+            fprintf(stderr, "connection-set-volume connection_id volume\n");
+            goto die;
+        }
+        uint16_t connection_id = 0;
+        float volume = 0;
+        errno = 0;
+        connection_id = strtoul(command_argv[0], NULL, 0);
+        if (errno != 0)
+        {
+            fprintf(stderr, "Expected unsigned integer, got %s\n", command_argv[0]);
+            goto die;
+        }
+        volume = strtof(command_argv[command_argc == 2 ? 1 : 0], NULL);
+        if (errno != 0 || volume < 0)
+        {
+            fprintf(stderr, "Expected floating point integer, got %s\n", command_argv[0]);
+            goto die;
+        }
+
+        if (autrans_connection_set_volume(socket, client_id, connection_id, volume) == 0)
+            print_volume(volume);
+    }
+    else if (strcasecmp(command, "output-get-volume") == 0)
+    {
+        uint16_t output_id = OBOS_AUD_DEFAULT_OUTPUT_DEV;
+        float volume = 0;
+        if (command_argc == 1)
+        {
+            errno = 0;
+            output_id = strtoul(command_argv[0], NULL, 0);
+            if (errno != 0)
+            {
+                fprintf(stderr, "Expected unsigned integer, got %s\n", command_argv[0]);
+                goto die;
+            }
+        }
+        if (autrans_output_get_volume(socket, client_id, output_id, &volume) == 0)
+            print_volume(volume);
+    }
+    else if (strcasecmp(command, "connection-set-volume") == 0)
+    {
+        if (command_argc < 1)
+        {
+            fprintf(stderr, "connection-set-volume connection_id\n");
+            goto die;
+        }
+        uint16_t connection_id = 0;
+        float volume = 0;
+        errno = 0;
+        connection_id = strtoul(command_argv[0], NULL, 0);
+        if (errno != 0)
+        {
+            fprintf(stderr, "Expected unsigned integer, got %s\n", command_argv[0]);
+            goto die;
+        }
+
+        if (autrans_connection_get_volume(socket, client_id, connection_id, &volume) == 0)
+            print_volume(volume);
+    }
+
+    die:
     autrans_disconnect(socket, client_id);
     shutdown(socket, SHUT_RDWR);
     close(socket);
